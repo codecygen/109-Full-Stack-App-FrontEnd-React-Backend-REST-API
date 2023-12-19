14c149c35c9ef1383c07aa1441ba96de0fbf0874
Aras <vahitaras@gmail.com>
2023-12-19 13:04:54 -0500

    first portion of the client react completed

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,9 +1,11 @@
+import classes from "./Body.module.scss";
+
 const Body = () => {
   return (
-    <>
+    <main className={classes.body}>
       <div>Body</div>
       <button>Aras</button>
-    </>
+    </main>
   );
 };
 
