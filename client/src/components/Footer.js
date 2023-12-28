882a3f2d20f86cee2aca70bc8bcacfedd85d8486
Aras <vahitaras@gmail.com>
2023-12-28 06:39:43 -0500

    footer is being worked on

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,15 @@
-import classes from "./Footer.module.scss"
+import classes from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>Footer</p>
+      <div className={classes.column1}>
+        <p>Aras</p>
+        <p>Aras</p>
+      </div>
+      <div className={classes.column2}>
+        <p>Aras2</p>
+      </div>
     </footer>
   );
 };
