dacba0141faade656d3c4a3ade2a67323d869c9a
Aras <vahitaras@gmail.com>
2023-12-28 07:21:06 -0500

    footer is finished. i will work on other forms and styling the event form

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFutbol } from "@fortawesome/free-solid-svg-icons";
+
 import classes from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.column1}>
-        <p>Aras</p>
-        <p>Aras</p>
+        <p>
+          <FontAwesomeIcon
+            icon={faFutbol}
+            size="xl"
+          />
+          {" "} Soccer Club
+        </p>
+        <p>
+          <a href="https://www.google.com">Latest Events</a>
+        </p>
+        <p>
+          <a href="https://www.google.com">Attendees</a>
+        </p>
       </div>
       <div className={classes.column2}>
-        <p>Aras2</p>
+        <p>
+          Created by{" "}
+          <strong>
+            <a href="https://aras-portfolio.web.app/">Vahit Aras Sen</a>
+          </strong>
+          , ©{new Date().getFullYear()}
+        </p>
       </div>
     </footer>
   );
