3c154eaf3c34107b27bbc0aa01e5880a4307ccbf
Aras <vahitaras@gmail.com>
2024-01-04 06:30:08 -0500

    clicking view button should activate getting db data

--- client/src/pages/details/:title:details/EventDetailsPage.js
+++ client/src/pages/details/:title:details/EventDetailsPage.js
@@ -1,15 +1,28 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+import DB from "../../../database/posts.json";
 
 import classes from "./eventDetailsPage.module.scss";
 
 const EventIdPage = () => {
   const params = useParams();
 
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    const foundData = DB.find(post => post._id === params.id);
+    setPostData(foundData);
+  }, [params.id]);
+
+  console.log(postData);
+
   return (
     <main className={classes.details}>
       <div>
-      <p>{params.title}</p>
-      <p>{params.id}</p></div>
+        <p>{params.title}</p>
+        <p>{params.id}</p>
+      </div>
     </main>
   );
 };
