9beec706a4d37d7b7a526b49f505e75806d57b89
Aras <vahitaras@gmail.com>
2024-01-04 06:07:08 -0500

    details page is being styled

--- client/src/pages/details/:title:details/EventDetailsPage.js
+++ client/src/pages/details/:title:details/EventDetailsPage.js
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 
+import classes from "./eventDetailsPage.module.scss";
+
 const EventIdPage = () => {
   const params = useParams();
 
   return (
-    <div>
+    <main className={classes.details}>
+      <div>
       <p>{params.title}</p>
-      <p>{params.id}</p>
-    </div>
+      <p>{params.id}</p></div>
+    </main>
   );
 };
 
