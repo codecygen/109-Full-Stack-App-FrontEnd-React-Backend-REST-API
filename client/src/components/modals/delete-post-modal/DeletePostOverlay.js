d18b8debf892290ec656b8305182b21d3277fdf2
Aras <vahitaras@gmail.com>
2024-01-11 06:56:00 -0500

    delete psot window closes successfully when the post is deleted

--- client/src/components/modals/delete-post-modal/DeletePostOverlay.js
+++ client/src/components/modals/delete-post-modal/DeletePostOverlay.js
@@ -1,26 +1,41 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { deletePost } from "../../../store/redux/utils/apiStateManagementsThunk";
+
+import { deletePostActions } from "../../../store/redux/delete-post-slice";
 
 import classes from "./DeletePostOverlay.module.scss";
 
 const NewPostOverlay = (props) => {
   const dispatch = useDispatch();
 
-  const deletePostData = useSelector(
-    (state) => state.deletePost.dataDeletePost
-  );
+  const {
+    dataDeletePost,
+    responseDeletePost,
+    errorDeletePost,
+    isLoadingDeletePost,
+  } = useSelector((state) => state.deletePost);
+
+  useEffect(() => {
+    // Only close errorpost window if the data deletion request
+    // successfully sent to backend
+    if (errorDeletePost === false && isLoadingDeletePost === false) {
+      dispatch(deletePostActions.toggleWindow());
+    }
+  }, [dispatch, errorDeletePost, isLoadingDeletePost]);
 
   const deleteButtonHandler = () => {
-    dispatch(deletePost(deletePostData._id));
+    dispatch(deletePost(dataDeletePost._id));
   };
 
   return (
     <section className={classes.box}>
       <header>
         <h1>Are you sure to delete?</h1>
+        <p>Problem</p>
       </header>
-      <p>{deletePostData.title}</p>
+      <p>{dataDeletePost.title}</p>
       <div>
         <button className={classes.button2} onClick={props.cancelFunc}>
           Cancel
