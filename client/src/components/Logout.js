f3b7ca9c314e93899f26a6a4fed0f0e32b91412d
Aras <vahitaras@gmail.com>
2024-02-15 15:46:38 -0500

    logout system works properly

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,15 +1,32 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import useAuth from "../hooks/use-auth";
+import { Avatar, Button } from "@mui/material";
+import { deepPurple } from "@mui/material/colors";
 
 const Logout = () => {
+  const navigate = useNavigate();
+
   const { name } = useAuth();
 
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("tokenName");
+    localStorage.removeItem("tokenExpiry");
+
+    navigate(0);
+  };
+
   return (
-    <>
-      <div>{name}</div>
-      <div>Logout</div>
-    </>
+    <main>
+      <p>Welcome {name}</p>
+      <Avatar sx={{ bgcolor: deepPurple[500] }}>a</Avatar>
+      <Button variant="contained" color="secondary" onClick={logoutHandler}>
+        Logout
+      </Button>
+    </main>
   );
 };
 
