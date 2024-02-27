cbcd4c6115b9eae9d51769fba1d4061c0f6c49d1
Aras <vahitaras@gmail.com>
2024-02-27 18:05:17 -0500

    added comment input section

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,27 +1,66 @@
-import { Button, Box, Typography, TextField } from "@mui/material";
+import { useState } from "react";
+import { Button, Box, TextField } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import SendIcon from "@mui/icons-material/Send";
 
 const CommentForm = () => {
+  const [comment, setComment] = useState("");
+  const [isCommentValid, setIsCommentValid] = useState(null);
+
   const defaultTheme = createTheme();
+
+  const changeHandler = (e) => {
+    const value = e.target.value;
+    setComment(value);
+
+    if (value.length <= 5) {
+      setIsCommentValid(false);
+    } else if (value.length > 5) {
+      setIsCommentValid(true);
+    }
+  };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
 
-    console.log("submitted");
+    if (!isCommentValid) {
+      console.log("Cannot submit!");
+      return;
+    }
+
+    console.log(comment);
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Box component="form" noValidate onSubmit={formSubmitHandler}>
-        <Typography component="p">Add a comment...</Typography>
+      <Box
+        component="form"
+        noValidate
+        onSubmit={formSubmitHandler}
+        sx={{ margin: "30px 0" }}
+      >
+        <TextField
+          required
+          fullWidth
+          margin="normal"
+          id="comment"
+          name="comment"
+          autoComplete="off"
+          multiline
+          rows={3}
+          onChange={changeHandler}
+          value={comment}
+          label={
+            isCommentValid === false ? "At Least 5 Characters" : "Add a Comment"
+          }
+          error={isCommentValid === false}
+        />
         <Button
           type="submit"
           variant="contained"
           sx={{
-            bgcolor: "red",
+            bgcolor: "#063d49",
             "&:hover": {
-              bgcolor: "red",
+              bgcolor: "#063d49",
             },
           }}
         >
